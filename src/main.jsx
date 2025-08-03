b1e193a3ab7aec897edcb724ec78b5632fd7dd77
dahyuniiiiii <imcute0703123@naver.com>
2025-08-04 02:22:06 +0900

    Feat: PWA 기능 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-
+import { registerSW } from "virtual:pwa-register";
+registerSW();
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
