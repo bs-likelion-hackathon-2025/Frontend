105d50f0205ca4bd78d4c231e3d5ed8071a71e23
dahyuniiiiii <imcute0703123@naver.com>
2025-08-04 01:18:06 +0900

    Fix: 푸터, 모달, 햄버거바 ui 디자인 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/common/Layout";
 import AiChat from "./pages/AiChat";
 import Home from "./pages/Home";
-
+import Shop from "./pages/Shop";
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/ai-chat" element={<AiChat />} />
+        <Route path="/shop" element={<Shop />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Shop() {
+  return (
+    <div className="homebg">
+      <div className="content">천안도감</div>
+    </div>
+  );
+}
+
+export default Shop;
