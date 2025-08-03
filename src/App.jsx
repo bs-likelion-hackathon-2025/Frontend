c50916e0bc5ef3de13e3285d9837949b3df7b1f8
dahyuniiiiii <imcute0703123@naver.com>
2025-08-03 21:48:57 +0900

    Del: 로딩페이지, 로그인페이지 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,11 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/common/Layout";
 import AiChat from "./pages/AiChat";
-import LoginPage from "./pages/LoginPage";
 import Home from "./pages/Home";
-import LoadingPage from "./pages/Loading/LoadingPage";
-import LoadingPage1 from "./pages/Loading/LoadingPage1";
-import LoadingPage2 from "./pages/Loading/LoadingPage2";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoadingPage />} />
-      <Route path="/loadingPage1" element={<LoadingPage1 />} />
-      <Route path="/loadingPage2" element={<LoadingPage2 />} />
-      <Route path="/loadingPage3" element={<LoginPage />} />
       <Route element={<Layout />}>
         <Route path="/home" element={<Home />} />
         <Route path="/ai-chat" element={<AiChat />} />
